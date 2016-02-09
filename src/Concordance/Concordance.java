bfd88d882efe1ae03efd53d342d3e6cf002aca58
Daniel Cregan <DKCREGAN@campus.uncg.edu>
2016-02-09 14:43:10 -0500

    Still need to get it to ignore punctuation as well as case and get it reading to a file otherwise basic functionality is good

--- src/Concordance/Concordance.java
+++ src/Concordance/Concordance.java
@@ -1,5 +1,39 @@
 package Concordance;
 
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Concordance {
-	
+
+    public static void main(String[] args) {
+        try {
+            String str;
+            HashMap Concordance = new HashMap();
+            Scanner s = new Scanner(new FileReader("Alice\'s Adventures in Wonderland.txt"));
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter a word to search for.");
+            str = in.nextLine();
+            int linenum = 1;
+            int numwordfound = 0;
+            while (s.hasNextLine()) {
+                String str3 = s.nextLine();
+                if (!str3.equals("")) {
+                    Scanner ss = new Scanner(str3);
+                    String str2 = ss.next();
+                    System.out.println(str2);
+                    if (str.equals(str2)) {
+                        numwordfound++;
+                        Concordance.put(linenum, numwordfound);
+                    }
+                }
+
+                linenum++;
+                System.out.println(linenum + " " + numwordfound);
+            }
+
+        } catch (Exception e) {
+            System.out.println("not found");
+        }
+    }
 }
