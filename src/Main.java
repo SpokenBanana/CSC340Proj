11b82bd07d297091122b76d5ff2d459a1a1fc46e
David Santana <santanavidda@gmail.com>
2016-02-09 15:02:05 -0500

    added basic commands

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) { 
+	/*
+		valid commands
+		-searchcon <name>
+		-searchbook <name>
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		IO.IO io = new IO.IO();
+		System.out.println("Enter command: ");
+		String commmand = scanner.nextLine();
+		String[] commands = commmand.split(" ");
+		if (commands[0].equals("-h")) {
+			System.out.println("Here are the commands\n" +
+					"-searchcon <concordance name> returns a list of saved concordances\n" +
+					"-serachbook <book name> returns a list of saved books.");
+		}
+		else if (commands[0].equals("-searchcon")) {
+			String s = "";
+			for (int i = 1;  i < commands.length; i++)
+				s += commands[i];
+			System.out.println(io.search_concordnace(s).toString());
+		}
+		else if (commands[0].equals("-searchbook")) {
+            String s = "";
+			for (int i = 1;  i < commands.length; i++)
+				s += commands[i];
+			System.out.println(io.get_book_list(s).size());
+		}
 	}
 }
